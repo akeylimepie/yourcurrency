052300be60bdb6441757bcd494829ce786f5058b
Олег <slash3k@gmail.com>
2017-01-17 18:18:29 +0500

    Добавлен шаблон файла настроек

--- daily.php
+++ daily.php
@@ -1,10 +1,8 @@
 <?php
-
 require_once __DIR__ . '/vendor/autoload.php';
 
 $yourCurrency = new \YourCurrency\Push(__DIR__);
 $yourCurrency->nextCover();
-
 $yourCurrency->dailyImage();
 $yourCurrency->updateLast();
 
